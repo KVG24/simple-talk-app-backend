7b9b4987dad2652c5a74ccd3624fc276af810600
KVG24 <141866132+KVG24@users.noreply.github.com>
2025-12-06 11:44:42 +0000

    Add authentication to routes

--- routes/router.js
+++ routes/router.js
@@ -1,21 +1,32 @@
 const { Router } = require("express");
+const passport = require("passport");
 const { validateSignUp } = require("../utils/validation.js");
 const authController = require("../controllers/authController.js");
 const messageController = require("../controllers/messageController");
 
 const router = Router();
+const authenticate = passport.authenticate("jwt", { session: false });
 
 router.post("/sign-up", validateSignUp, authController.registerProfile);
 router.post("/log-in", authController.logIn);
 
-router.get("/messages/:receiverId", messageController.getMessagesByReceiverId);
-router.get("/messages/:senderId", messageController.getMessagesBySenderId);
+router.get(
+    "/messages/:receiverId",
+    authenticate,
+    messageController.getMessagesByReceiverId
+);
+router.get(
+    "/messages/:senderId",
+    authenticate,
+    messageController.getMessagesBySenderId
+);
 router.get(
     "/messages/:senderId/:receiverId",
+    authenticate,
     messageController.getConversation
 );
-router.post("/messages/create", messageController.createMessage);
-router.put("/messages/:id", messageController.editMessage);
-router.delete("/messages/:id", messageController.deleteMessage);
+router.post("/messages/create", authenticate, messageController.createMessage);
+router.put("/messages/:id", authenticate, messageController.editMessage);
+router.delete("/messages/:id", authenticate, messageController.deleteMessage);
 
 module.exports = router;
